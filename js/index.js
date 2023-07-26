c9abfc6296daa72339d8b21b2470087d0b24fa77
Sinthia-22 <sinthiasupti@gmail.com>
2023-07-26 16:12:41 +0600

    added new field

---
+++ js/index.js
@@ -0,0 +1,17 @@
+console.log("Hi! From first JS");
+
+// alert("Hi! From first JS");
+
+
+let floatNumber = 2;
+
+let age1 = 3;
+let firstName= "Supti";
+age1=age1.toString();
+ document.write(floatNumber + "<br>");
+ document.write("<span style ='color:skyblue ;  font-size:100px '>" + floatNumber + "<br></span>"); 
+ document.write(age1); 
+ console.log(window);
+ console.log(typeof(age1));
+ console.log(typeof(floatNumber));
+ console.log(typeof(firstName));
